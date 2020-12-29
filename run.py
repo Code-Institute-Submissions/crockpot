0123dae9e2e31c5889a54b0a5ffd4f8dd7af43b4
zoet24 <zt12542@my.bristol.ac.uk>
2020-12-29 19:20:37 +0000

    Link all html pages to base.html

--- run.py
+++ run.py
@@ -15,6 +15,26 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/addRecipe")
+def addRecipe():
+    return render_template("addRecipe.html")
+
+
+@app.route("/searchRecipe")
+def searchRecipe():
+    return render_template("searchRecipe.html")
+
+
+@app.route("/shoppingList")
+def shoppingList():
+    return render_template("shoppingList.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
