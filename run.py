c741dc3d37c41e17e5dda8232e1e5a2bb4b624e9
zoet24 <zt12542@my.bristol.ac.uk>
2021-01-10 21:20:22 +0000

    Add logout functionality

--- run.py
+++ run.py
@@ -54,6 +54,14 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -85,7 +93,11 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 @app.route("/addRecipe")
