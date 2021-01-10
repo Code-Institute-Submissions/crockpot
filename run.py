c3434bb9c4ad444ff4adf38cf63e8f85be728e57
zoet24 <zt12542@my.bristol.ac.uk>
2021-01-10 21:06:32 +0000

    Add profile name = user session name

--- run.py
+++ run.py
@@ -23,13 +23,34 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("input-username-login").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get(
+                    "input-password-login")):
+                    session["user"] = request.form.get(
+                        "input-username-login").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("input-username-login")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
@@ -54,7 +75,17 @@ def signup():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("input-username-signup").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("signup.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 @app.route("/addRecipe")
