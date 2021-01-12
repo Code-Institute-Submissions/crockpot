5f7760fd71ab632f03cdf4c10913d5391fc88979
zoet24 <zt12542@my.bristol.ac.uk>
2021-01-12 21:27:48 +0000

    Reassess styling across entire site

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,17 @@
-// Slider value display
+// Show serving slider value
 var sliderServes = document.getElementById("serves");
 var outputServes = document.getElementById("serves-value");
-
-var sliderCookingTime = document.getElementById("cooking_time");
-var outputCookingTime = document.getElementById("cooking_time-value");
-
 outputServes.innerHTML = sliderServes.value;
-outputCookingTime.innerHTML = sliderCookingTime.value;
 
 sliderServes.oninput = function() {
   outputServes.innerHTML = this.value;
 }
-sliderCookingTime.oninput = function() {
-  outputCookingTime.innerHTML = this.value;
+
+// Show cooking time slider value
+var sliderCookTime = document.getElementById("cooktime");
+var outputCookTime = document.getElementById("cooktime-value");
+outputCookTime.innerHTML = sliderCookTime.value;
+
+sliderCookTime.oninput = function() {
+  outputCookTime.innerHTML = this.value;
 }
