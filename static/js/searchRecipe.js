55f22fdd8290b5a447efb17583847e17e8580ba7
zoet24 <zt12542@my.bristol.ac.uk>
2021-02-04 10:27:41 +0000

    Update informative text bubbles across website

--- static/js/searchRecipe.js
+++ static/js/searchRecipe.js
@@ -2,17 +2,13 @@
 function searchIngredients() {
     var ingredientsList = document.getElementById("ingredients-list");
     var ingredientsCheckbox = ingredientsList.getElementsByTagName("input");
-    var ingredientsString = "";
     var ingredientsStringChecked = "";
 
     for (var i = 0; i < ingredientsCheckbox.length; ++i) {
-        ingredientsString2 = ingredientsCheckbox[i].value
+        ingredientsString = ingredientsCheckbox[i].value
 
         if (ingredientsCheckbox[i].checked) {
-            ingredientsStringChecked = ingredientsStringChecked.concat(ingredientsString2 + " ");
-        }
-        else {
-            ingredientsString = ingredientsString.concat(ingredientsString2 + " ");
+            ingredientsStringChecked = ingredientsStringChecked.concat(ingredientsString + " ");
         }
     }
 
