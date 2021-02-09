84dcb296fe11f9f47b436af053ffe58cf8237470
zoet24 <zt12542@my.bristol.ac.uk>
2021-02-09 17:06:21 +0000

    Bug #2 fix: Slicing issue on searchRecipe.html resolved

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,31 @@
 // Modal controls
-    // function openModalView() {
-    //     idNumModal = document.getElementById("modal-delete");
-    //     $(idNumModal).removeClass("hidden");
-    // }
+function openModalView() {
+    idNumModal = document.getElementById("modal-delete");
+    $(idNumModal).removeClass("hidden");
+}
 
-    // function closeModalView() {
-    //     idNumModal = document.getElementById("modal-delete");
-    //     $(idNumModal).addClass("hidden");
-    // }
+function closeModalView() {
+    idNumModal = document.getElementById("modal-delete");
+    $(idNumModal).addClass("hidden");
+}
 
 // Non-profile
 function openModal() {
     var target = event.target || event.srcElement;
     var id = target.id;
-    var idNum = id.slice(-1);
+    var idNum = id.match(/\d+/)[0];
     var idNumModal = document.getElementById("modal-delete" + idNum);
 
+    // console.log(id);
+    // console.log(idNum);
+    // console.log(id.match(/\d+/)[0]);
     $(idNumModal).removeClass("hidden");
 }
 
 function closeModal() {
     var target = event.target || event.srcElement;
     var id = target.id;
-    var idNum = id.slice(-1);
+    var idNum = id.match(/\d+/)[0];
     var idNumModal = document.getElementById("modal-delete" + idNum);
 
     $(idNumModal).addClass("hidden");
@@ -32,7 +35,7 @@ function closeModal() {
 function openModalMy() {
     var target = event.target || event.srcElement;
     var id = target.id;
-    var idNum = id.slice(-1);
+    var idNum = id.match(/\d+/)[0];
     var idNumModal = document.getElementById("modal-delete-my-recipe" + idNum);
 
     $(idNumModal).removeClass("hidden");
@@ -41,7 +44,7 @@ function openModalMy() {
 function closeModalMy() {
     var target = event.target || event.srcElement;
     var id = target.id;
-    var idNum = id.slice(-1);
+    var idNum = id.match(/\d+/)[0];
     var idNumModal = document.getElementById("modal-delete-my-recipe" + idNum);
 
     $(idNumModal).addClass("hidden");
@@ -50,7 +53,7 @@ function closeModalMy() {
 function openModalFav() {
     var target = event.target || event.srcElement;
     var id = target.id;
-    var idNum = id.slice(-1);
+    var idNum = id.match(/\d+/)[0];
     var idNumModal = document.getElementById("modal-delete-fav-recipe" + idNum);
 
     $(idNumModal).removeClass("hidden");
@@ -59,7 +62,7 @@ function openModalFav() {
 function closeModalFav() {
     var target = event.target || event.srcElement;
     var id = target.id;
-    var idNum = id.slice(-1);
+    var idNum = id.match(/\d+/)[0];
     var idNumModal = document.getElementById("modal-delete-fav-recipe" + idNum);
 
     $(idNumModal).addClass("hidden");
